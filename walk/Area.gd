88764f78ddf9ffc9a21696e0d282cc7a22d495ed
prlprncs <84919072+prlprncs@users.noreply.github.com>
2023-03-23 10:28:52 +0800

    npc not workiing

---
+++ walk/Area.gd
@@ -0,0 +1,31 @@
+extends Area
+
+var dialogue = "What is the capital of France?"
+var answer = "Paris"
+var interact_text = "Press 'E' to interact"
+
+
+
+onready var player = get_node("/root/player")
+
+func _ready():
+	var interact_label = Label.new()
+	interact_label.text = interact_text
+	interact_label.align = Label.ALIGN_CENTER
+	interact_label.rect_position = Vector2(0, -50)
+	add_child(interact_label)
+
+func _input(event):
+	if event.is_action_pressed("ui_accept") and player.global_transform.origin.distance_to(global_transform.origin) < 5.0:
+		var question_box = load("res://DialogBox.tscn").instance()
+		get_tree().get_root().add_child(question_box)
+		question_box.set_text(dialogue)
+		question_box.connect("answered", self, "_on_answered")
+
+func _on_answered(player_answer):
+	if player_answer == answer:
+		print("Correct!")
+	else:
+		print("Wrong answer.")
+
+
